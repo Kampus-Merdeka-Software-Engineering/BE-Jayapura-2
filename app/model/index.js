4447ae2932b5a143a7f40fc78fd3b46f74c26a29
ImamGozali <imamgozali.b@gmail.com>
2023-10-05 21:35:10 +0700

    update index.js model

--- app/model/index.js
+++ app/model/index.js
@@ -1,10 +1,12 @@
 const sequelize = require('sequelize')
 const mysql = require('mysql2')
+const fs = require('fs')
+const path = require('path')
 
-const connection = new sequelize.Sequelize('nuansa_batik', 'root', 'root', {
-    host: 'localhost',
+const conn = new sequelize.Sequelize("mysql://avnadmin:AVNS_mHLMfnQ4yQhrp1XcpTh@mysql-e53703d-capstoneprojectgroup2.aivencloud.com:25949/nuansa_batik?ssl-mode=REQUIRED", {
+    ssl: fs.readFileSync(path.join(__dirname, './ca.pem')),
     dialect: 'mysql',
     logging: false
-})
+});
 
-module.exports = connection
+module.exports = conn
