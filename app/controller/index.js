86caa92327b03b910ded9b54f346ccc558b09b92
Sigit Arif Anggoro <sigit.arifanggoro@advotics.com>
2023-10-08 14:12:50 +0700

    add mapListProduct

--- app/controller/index.js
+++ app/controller/index.js
@@ -6,7 +6,17 @@ const router = express.Router();
 function listProduct(req, res, next) {
     product.findAll()
         .then(function (data) {
-            res.json(data);
+        //    Map the response data to the desired format
+        const mappedData = data.map((product) => ({
+            id: product.id,
+            name: product.nama,
+            image: product.image,
+            price: product.harga,
+            listImage: JSON.parse(product.image_list.replace(/'/g, '"')), // parse to array JS
+            ukuran: JSON.parse(product.ukuran.replace(/'/g, '"')) // parse to array JS
+      }));
+      
+        res.json(mappedData);
         })
         .catch(function (err) {
             res.json({
