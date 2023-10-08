46786f3e4fabc0882483a07aaba9cc1688441a0b
Brian Khaw <briankhaw125@gmail.com>
2023-10-08 13:41:09 +0700

    Update BE

--- app/routes.js
+++ app/routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require("./controller/index")
 const loginController = require('./controller/loginController')
+const contactus = require('./controller/contactus')
 
 // Endpoint untuk login
 router.post('/login', loginController.login)
 router.post('/signup', loginController.signup);
 
 router.get('/products', controller.listProduct)
-router.post('/contact', controller.contact)
+router.post('/contact', contactus.contactus)
 
 module.exports = router;
