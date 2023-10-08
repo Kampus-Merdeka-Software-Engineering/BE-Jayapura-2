ad80fcccbe7ed3b45ad7040bf4337327626c2ada
Brian Khaw <briankhaw125@gmail.com>
2023-10-08 15:35:48 +0700

    Update BE

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,7 @@ const contactus = require('./controller/contactus')
 
 // Endpoint untuk login
 router.post('/login', loginController.login)
-router.post('/signup', loginController.signup);
+router.post('/signup', loginController.signup)
 
 router.get('/products', controller.listProduct)
 router.post('/contact', contactus.contactus)
