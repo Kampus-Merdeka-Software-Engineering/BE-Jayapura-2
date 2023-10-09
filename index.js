42376ca962291470306ea84e3d34fb1b721bd5e6
ImamGozali <imamgozali.b@gmail.com>
2023-10-09 14:24:51 +0700

    update index, package-lock dan package

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const express = require("express") // ini perlu
-const cors = require('cors')
-const path = require('path'); //tidak perlu npm install
-const connection = require('./db')
-
-
-// init express server and router
-const app = express();
+const express = require("express");
+const cors = require('cors');
+const helmet = require('helmet'); // Tambahkan helmet
+const connection = require('./db');
 const mainRouter = require('./app/routes');
 
-app.use(express.json()); // supaya express bisa response json
-app.use(express.urlencoded({ extended: false })); // supaya express bisa menerima body
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-// http router
+app.use(helmet()); // Gunakan middleware helmet
+
 app.use("/", mainRouter);
 
-const port = 3333
-app.listen(port, function(){
-    console.log("server start on", port)
+const port = 3333;
+app.listen(port, function () {
+    console.log("Server started on", port);
     connection.authenticate()
-    .then(function(){
-        console.log("Database terhubung")
-    })
-    .catch(function(err){
-        console.log("Error saat koneksi ke database", err)
-        process.exit()
-    })
-})
+        .then(function () {
+            console.log("Database terhubung");
+        })
+        .catch(function (err) {
+            console.error("Error saat koneksi ke database", err);
+            process.exit(1); // Perbaiki penanganan kesalahan
+        });
+});
